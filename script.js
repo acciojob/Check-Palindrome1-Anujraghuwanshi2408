9cf75f20249152e039fbc0efe8c3a2357767138c
Anujraghuwanshi2408 <128280445+Anujraghuwanshi2408@users.noreply.github.com>
2023-05-20 13:06:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 // complete the given function
-
+ let isPalindrome = true;
+ let start = 0;
+let end  = str.length-1;
 function palindrome(str){
+	while(start<=end && isPalindrome){
+		if(str[start] != str[end]){
+			isPalindrome = false;
+		}
+		start++;
+		end--;
+	}
+	return (isPalindrome ? true:false);
 
 }
 module.exports = palindrome
